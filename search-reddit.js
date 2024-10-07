f77a426914068b3e8a492470056d5b8fee589934
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-10-07 07:42:10 -0500

    Add toggling to search Reddit.

--- search-reddit.js
+++ search-reddit.js
@@ -1,5 +1,19 @@
-(() => {
-  var p = new URLSearchParams(location.search);
-  p.set("q", (p.get("q") || "") + " site:reddit.com");
-  location.search = "" + p;
-})();
+/**
+ * @param {string} term
+ * @param {string} searchParamName
+ * @param {string} separator
+ */
+function toggleSearchTerm(term, searchParamName, separator) {
+  const searchParams = new URLSearchParams(location.search);
+  const q = searchParams.get(searchParamName).split(separator);
+  const i = q.indexOf(term);
+  if (i >= 0) {
+    q.splice(i, 1);
+  } else {
+    q.push(term);
+  }
+  searchParams.set(searchParamName, q.join(separator));
+  location.search = "" + searchParams;
+}
+
+toggleSearchTerm("site:reddit.com", "q", " ");
